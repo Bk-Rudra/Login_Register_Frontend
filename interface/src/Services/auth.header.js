e4deb5ac10643778c4103dc16d2d521de8b19b44
Bhupendra Kumar <bhuppi.rudra@gmail.com>
2021-11-09 18:00:57 +0530

    file changes

--- interface/src/Services/auth.header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-  const user = JSON.parse(localStorage.getItem("user"));
-
-  if (user && user.accessToken) {
-    return { Authorzation: "Bearer" + user.accessToken }; //for spring boot back-end
-  } else {
-    return {};
-  }
-}
